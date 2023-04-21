a2fcdcc5392c14cbbfa78256c52c1e82c3d6d25b
sundaray <pgcim14.hemant@spjimr.org>
2023-04-21 22:09:48 +0530

    styling changed

--- src/components/post/Heading1.js
+++ src/components/post/Heading1.js
@@ -8,12 +8,12 @@ const Heading1 = ({ children, date }) => {
     <div className="mt-12 ">
       <Link
         href="/blog"
-        className="w-24 pb-12 pl-2 all-posts-link flex items-center"
+        className="w-24 pl-2 all-posts-link flex items-center bg-zinc-50 rounded-full"
       >
         <ArrowLeftIcon className="arrow-left w-4 h-4 text-orange-500" />
         <p className="text-orange-500">All posts</p>
       </Link>
-      <div className="bg-zinc-50 px-2 border-b-2 space-y-4 py-6">
+      <div className="px-2 border-b-2 space-y-4 pb-6 pt-12">
         <Breadcrumbs />
         <h1 className="text-4xl md:text-6xl text-gray-900 font-bold">
           {children}{" "}
